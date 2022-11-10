b672bba791572a4dc6a8a734f27538265a679e56
Boluwatife Mustapha <mustaphaboluwatife01@gmail.com>
2022-11-10 14:31:45 +0100

    Initial commit

---
+++ android/app/src/main/java/com/example/restcountries_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.restcountries_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/utils/methods.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+class fetchdata{
+  Future<dynamic> get (String api) async{
+    var client = http.Client();
+
+  }
+}
